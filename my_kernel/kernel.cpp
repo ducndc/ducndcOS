071e6ae23578a9f297d372f8ca4b2732554ae844
ducndc <nguyendangchungduc1999@gmail.com>
2023-08-16 09:12:12 +0700

    [OS-2]<feature>[Memory]: Update memory segment and global descriptior table

--- my_kernel/kernel.cpp
+++ my_kernel/kernel.cpp
@@ -1,7 +1,7 @@
 #include "types.h"
+#include "gdt.h"
 
-
-void printf(char* str)
+void print(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
@@ -12,7 +12,9 @@ void printf(char* str)
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
-    printf("Hello World! ---- http://www.AlgorithMan.de");
+    print("Wellcome to ducndcOS <ducndc.github.io>");
     
+    GlobalDescriptorTable gdt;
+
     while(1);
 }
